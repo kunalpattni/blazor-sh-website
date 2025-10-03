82730a28139e85e9eb91265beb1a5daab887212b
Kunal <kpattni@protonmail.com>
2025-10-03 08:39:56 +0100

    ball animation

---
+++ BlazorShWebsite/NotifiableBool.cs
@@ -0,0 +1,40 @@
+using System.ComponentModel;
+
+namespace BlazorShWebsite;
+
+public class NotifiableBool : INotifyPropertyChanged
+{
+    public NotifiableBool(bool value)
+    {
+        _value = value;
+    }
+    
+    private bool _value;
+    public bool Value
+    {
+        get => _value;
+        set
+        {
+            if (_value == value) return;
+            _value = value;
+            PropertyChanged?.Invoke(this, new(nameof(Value)));
+        }
+    }
+    
+    public Task WaitForAnyChangeAsync()
+    {
+        var tcs = new TaskCompletionSource();
+
+        PropertyChangedEventHandler handler = null!;
+        handler = (s, e) =>
+        {
+            PropertyChanged -= handler;
+            tcs.TrySetResult();
+        };
+
+        PropertyChanged += handler;
+        return tcs.Task;
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+}
